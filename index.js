ce2b9692db40963a0131feb6e15b57d55f0dedce
Cieslik85 <cieslik85@gmail.com>
2024-06-09 19:28:44 +0100

    add styling and make post request functional

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+const artList = [];
+let currentDate;
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  
+    res.render("index.ejs", { artList: artList, currentDate: currentDate });
   });
 
+app.get("/write", (req, res) => {
+  if (!currentDate) {
+    currentDate = new Date().toLocaleString();
+  }
+  res.render("write-blog.ejs");
+});
 
+app.post("/submit", (req, res) => {
 
+  const { atitle, acontent } = req.body;
 
+  const newArt = {
+    title: capitalizeFirstLetter(atitle),
+    content: capitalizeFirstLetter(acontent),
+    date: new Date().toLocaleString(),
+  };
 
+  artList.push(newArt);
+
+  res.redirect("/");
+  });
   
+  app.put("/write/:id", (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+  
+    // Actualizar la publicación correspondiente en el array de posts
+    artList[id].title = title;
+    artList[id].content = content;
+  
+    res.sendStatus(200);
+  });
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
